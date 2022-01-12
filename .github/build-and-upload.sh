dd7f173a5624b5924576dc3a089aa9ff3d32a736
HarelM <harel.mazor@gmail.com>
2022-01-12 21:53:59 +0200

    Resolves #8 - Add graphhopper.sh back to the image

--- .github/build-and-upload.sh
+++ .github/build-and-upload.sh
@@ -3,8 +3,9 @@
 echo "Cloning graphhopper"
 git clone https://github.com/graphhopper/graphhopper.git
 cd graphhopper
-echo "Downloading Dockerfile"
+echo "Downloading Dockerfile and graphhopper.sh"
 curl -L https://raw.githubusercontent.com/IsraelHikingMap/graphhopper-docker-image-push/main/Dockerfile > Dockerfile
+curl -L https://raw.githubusercontent.com/IsraelHikingMap/graphhopper-docker-image-push/main/graphhopper.sh > graphhopper.sh
 echo "Building docker image"
 docker build . -t israelhikingmap/graphhopper:latest
 docker login --username $DOCKERHUB_USER --password $DOCKERHUB_TOKEN
@@ -16,8 +17,10 @@ if docker manifest inspect israelhikingmap/graphhopper:$TAG >/dev/null; then
     echo "No need to publish existing version: $TAG";
 else
     mv Dockerfile ../Dockerfile
+    mv graphhopper.sh ../graphhopper.sh
     git checkout tags/$TAG
     mv ../Dockerfile Dockerfile
+    mv ../graphhopper.sh graphhopper.sh
     echo "Building docker image for tag: $TAG"
     docker build . -t israelhikingmap/graphhopper:$TAG
     echo "Publishing docker image for tag: $TAG"
