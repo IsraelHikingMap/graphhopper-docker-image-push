f06f2beb4887efdcb3165cabf3012b6cc86ad97c
zstadler <zeev.stadler@gmail.com>
2023-01-12 11:24:39 +0200

    Build scripts refactoring (#33)
    * Build scripts refactoring
    
    `.github/build-and-upload.sh` to use `build.sh`
    Related to  #32
    
    * Update build.sh
    
    Following review comments

--- build.sh
+++ build.sh
@@ -2,18 +2,31 @@
 
 usage() (
 cat <<USAGE
-Build a docker image
+Build a docker image for GraphHopper and optionally push it to Docker Hub
 
 Usage:
-  ./build.sh [<tag> | --help]
+  ./build.sh [[--push] <tag>]
+  ./build.sh --help
 
 Argument:
-  <tag>   Build an image for the given graphhopper repository tag [default: master]
+  <tag>         Build an image for the given graphhopper repository tag [default: master]
 
 Option:
-  --help  Print this message
+  --push        Push the image to Docker Hub
+  --help        Print this message
+  
+Docker Hub credentials are needed for pushing the image. If they are not provided using the
+DOCKERHUB_USER and DOCKERHUB_TOKEN environment variables, then they will be asked interactively.
 USAGE
 )
+
+if [ "$1" == "--push" ]; then
+  push="true"
+  docker login --username "${DOCKERHUB_USER}" --password "${DOCKERHUB_TOKEN}" || exit $?
+  shift
+else
+  push="false"
+fi
 
 if [ $# -gt 1 ] || [ "$1" == "--help" ]; then
   usage
@@ -23,6 +36,9 @@ fi
 if [ ! -d graphhopper ]; then
   echo "Cloning graphhopper"
   git clone https://github.com/graphhopper/graphhopper.git
+else
+  echo "Pulling graphhopper"
+  (cd graphhopper; git checkout master; git pull)
 fi
 
 imagename="israelhikingmap/graphhopper:${1:-latest}"
@@ -32,8 +48,10 @@ if [ "$1" ]; then
 fi
 
 echo "Building docker image ${imagename}"
-docker build . -t ${imagename}
+docker build . -t "${imagename}"
 
-if [ $# -eq 1 ]; then
+if [ "${push}" == "false" ]; then
   echo "Use \"docker push ${imagename}\" to publish the image on Docker Hub"
+else
+  docker push "${imagename}"
 fi
